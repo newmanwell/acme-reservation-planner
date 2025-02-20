2eb51894c7d6c12f35bc7ce14130d145677bd2b5
Shaun Newman <shaun.newman.p@gmail.com>
2025-02-20 17:04:41 -0500

    Tables are successfully adding and dropping from DB

--- db/seed.js
+++ db/seed.js
@@ -1,5 +1,16 @@
 const client = require('./client.js');
 
+const dropTables = async() => {
+  try {
+    await client.query(`
+      DROP TABLE IF EXISTS reservation;
+      DROP TABLE IF EXISTS customer;
+      DROP TABLE IF EXISTS restaurant;
+      `)
+  } catch(error) {
+    console.log(error);
+  }
+}
 
 const createTables = async() => {
   try {
@@ -32,12 +43,13 @@ const syncAndSeed = async() => {
     await client.connect();
     console.log('Connected to acme_reservation DB');
 
+    console.log('Deleting Tables')
+    await dropTables();
+    console.log('Tables Deleted')
+
     console.log('Creating Tables');
     await createTables();
     console.log('Tables Created');
-
-
-
 
     await client.end();
     console.log('Disconnected from DB');
