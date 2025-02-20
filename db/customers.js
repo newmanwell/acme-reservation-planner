5db1e7854dc96c4f503b14b17341e2d08c373e8c
Shaun Newman <shaun.newman.p@gmail.com>
2025-02-20 17:43:11 -0500

    createCustomer function created

---
+++ db/customers.js
@@ -0,0 +1,18 @@
+const client = require('./client.js');
+
+const createCustomer = async(customerName) => {
+  try {
+    const { rows } = await client.query(`
+      INSERT INTO customers (name)
+      VALUES ('${customerName}')
+      RETURNING *;
+      `);
+      const customer = rows[0];
+      return customer;
+    console.log(x);
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+module.exports = { createCustomer };
