d1a442cc74ea5fef4ba169aae609c695c090cd2a
Shaun Newman <shaun.newman.p@gmail.com>
2025-02-22 15:46:40 -0500

    NewCustomer component added and returns an h2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Link } from 'react-router-dom';
 
 import CustomerList from './components/Customers.jsx';
+import NewCustomer from './components/NewCustomer.jsx';
 
 const App = () => {
 
@@ -8,11 +9,14 @@ const App = () => {
     <>
       <h1>Front End!</h1>
       <nav>
+        <Link to='/'>Home</Link>
         <Link to="/customers">See Customers</Link>
+        <Link to="/newcustomer">Add New Customer</Link>
       </nav>
       <div>
         <Routes>
-          <Route path="/customers" element={<CustomerList />}/>
+          <Route path="/customers" element={<CustomerList />} />
+          <Route path="/newcustomer" element={<NewCustomer />} />
         </Routes>
       </div>
     </>
---
+++ src/components/NewCustomer.jsx
@@ -0,0 +1,8 @@
+
+const NewCustomer = () => {
+  return (
+    <h2>New Customer Form</h2>
+  )
+}
+
+export default NewCustomer;
